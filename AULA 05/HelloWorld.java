a4063744b7d31b72111240f98e622595bfe22187
Tiago Tas <tiagotas@live.com>
2015-12-10 16:36:09 -0200

    Revert "Revert "add HelloWorld.java""
    This reverts commit 13baf5a11e670e963a97a8641f4f4442036a5602.

---
+++ AULA 05/HelloWorld.java
@@ -0,0 +1,15 @@
+//Declaração da Classe
+//Nome da Classe: HelloWorld
+class HelloWorld {
+
+//Método da classe
+//Public static - modificadores de acesso no método
+//void - tipo de retorno do método
+//main - nome do método
+//() Argumentos do método
+	public static void main (String[] args){
+
+//Nosso código 
+	System.out.println("Hello World!");
+	}
+}
